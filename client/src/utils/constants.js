4820dae850a5e6c0364f5a128ad0a9b69384be93
Alok Kumar Rathod <alok@Aloks-MacBook-Air.local>
2025-07-26 14:45:12 +0530

    starting new

---
+++ client/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const USER_API_END_POINT = "http://localhost:8080/api/user";
+export const PRODUCT_API_END_POINT = "http://localhost:8080/api/product";
+export const REQUEST_API_END_POINT = "http://localhost:8080/api/request";
